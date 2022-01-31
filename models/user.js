05ca460ebc14a14257aaee89bc51b3039ee6f781
cogilvy <chrisogilvy@cox.net>
2022-01-31 14:15:45 -0800

    Crud Helper

--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const SALT_ROUNDS = 6;
 
 const userSchema = new Schema({
   name: {type: String, required: true},
@@ -25,5 +28,13 @@ const userSchema = new Schema({
     }
   }
 });
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  // update the password field with a hash
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  return next();
+});
+
 
 module.exports = mongoose.model('User', userSchema);
