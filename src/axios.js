7203f5a9fcc74e396559505ed22f44bd55adeb0f
lejt <92820402+lejt@users.noreply.github.com>
2022-03-07 21:42:52 -0800

    deploy to firebase front end and back end with package.json fix

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     // API url (cloud function)
-    baseURL: 'http://localhost:5001/clone-84264/us-central1/api'
+    baseURL: 'https://us-central1-clone-84264.cloudfunctions.net/api'
+    // baseURL: 'http://localhost:5001/clone-84264/us-central1/api'
 });
 
 export default instance;
